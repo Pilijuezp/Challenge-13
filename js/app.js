c40b78f950a6da06d1ef87565a264d025c1ecb27
Pilar Juez Perez <pilijuezp@gmail.com>
2022-04-29 14:56:25 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let number1=Number(prompt('Elegi el primer numero'));
+let number2=Number(prompt('Elegi el segundo numero'));
+if(number1>number2){
+    console.log('El primer numero es mayor que el segundo');
+}else if (number1==number2){
+    console.log('El primer numero es igual que el segundo');
+}else if(number1<number2){
+    console.log('El primer numero es menor que el segundo');
+}
